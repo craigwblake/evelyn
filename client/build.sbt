2d0f66f3d9b46210a51488346bf7135058d916ac
Craig Blake <craigwblake@gmail.com>
2014-07-01 19:00:43 -0400

    Added jquery.

--- client/build.sbt
+++ client/build.sbt
@@ -6,10 +6,12 @@ ScalaJSKeys.persistLauncher := true
 
 ScalaJSKeys.persistLauncher in Test := false
 
-//libraryDependencies += "org.webjars" % "jquery" % "2.1.1"
+skip in ScalaJSKeys.packageJSDependencies := false
+
+libraryDependencies += "org.webjars" % "jquery" % "2.1.1"
+
+libraryDependencies += "org.scala-lang.modules.scalajs" %%% "scalajs-jquery" % "0.6"
 
 libraryDependencies += "org.scala-lang.modules.scalajs" %%% "scalajs-dom" % "0.6"
-
-//libraryDependencies += "org.scalajs" %%% "scalajs-pickling" % "0.3"
 
 libraryDependencies += "org.scala-lang.modules.scalajs" %% "scalajs-jasmine-test-framework" % scalaJSVersion % "test"
--- client/src/main/scala/Canvas.scala
+++ client/src/main/scala/Canvas.scala
@@ -2,6 +2,7 @@ package evelyn
 
 import org.scalajs.dom
 import org.scalajs.dom.document
+import org.scalajs.jquery._
 import scala.scalajs.js
 import scala.scalajs.js._
 
@@ -15,12 +16,25 @@ object Canvas extends JSApp {
 
 	def main: Unit = {
 		var canvas = document.getElementById("board").asInstanceOf[HTMLCanvasElement]
-		implicit var context = canvas.getContext("2d")
+		implicit var c = canvas.getContext("2d")
+
 		val (arrow, orientation, startCoords, targetCoords) = initializeBoard
+
+		val button = jQuery("#submit")
+		val textarea = jQuery("#script")
+
+		def handler = () => { runScript(textarea, arrow, orientation, startCoords, targetCoords) }
+
+		button.click { handler _ }
+	}
+
+	def runScript (textarea: JQuery, arrow: Image, orientation: Orientation, startCoords: (Int, Int), targetCoords: (Int, Int))(implicit c: CanvasRenderingContext2D) = {
+		val script = textarea.text
+		println(s"script $script")
 	}
 
 
-	def initializeBoard (implicit context: CanvasRenderingContext2D): (Image, Orientation, (Int, Int), (Int, Int)) = {
+	def initializeBoard (implicit c: CanvasRenderingContext2D): (Image, Orientation, (Int, Int), (Int, Int)) = {
 
 		val orientation = Orientation(Math.round(Math.random * 3).toInt)
 		println(s"orientation: $orientation")
@@ -51,51 +65,51 @@ object Canvas extends JSApp {
 		} else return startCoords
 	}
 
-	def drawBoard (arrow: Image, orientation: Orientation, arrowCoords: (Int, Int), targetCoords: (Int, Int))(implicit context: CanvasRenderingContext2D): Unit = {
+	def drawBoard (arrow: Image, orientation: Orientation, arrowCoords: (Int, Int), targetCoords: (Int, Int))(implicit c: CanvasRenderingContext2D): Unit = {
 		val p = 50
 
-		context.save()
-		context.beginPath()
-		context.strokeStyle = "black"
+		c.save()
+		c.beginPath()
+		c.strokeStyle = "black"
 		for (i <- 0 to 700 by 70) {
-			context.moveTo(i + p, p)
-			context.lineTo(i + p, p + 700)
+			c.moveTo(i + p, p)
+			c.lineTo(i + p, p + 700)
 		}
 		for (i <- 0 to 700 by 70) {
-			context.moveTo(p, i + p)
-			context.lineTo(p + 700, i + p)
+			c.moveTo(p, i + p)
+			c.lineTo(p + 700, i + p)
 		}
-		context.stroke()
-		context.restore()
+		c.stroke()
+		c.restore()
 		drawTarget(targetCoords._1 * 70, targetCoords._2 * 70)
 		drawArrow(arrowCoords._1 * 70, arrowCoords._2 * 70, arrow, orientation)
 	}
 
-	def drawTarget (x: Int, y: Int)(implicit context: CanvasRenderingContext2D) = {
-		context.save()
-		context.beginPath()
-		context.rect(x - 20, y - 20, 70, 70)
-		context.fill()
-		context.restore()
+	def drawTarget (x: Int, y: Int)(implicit c: CanvasRenderingContext2D) = {
+		c.save()
+		c.beginPath()
+		c.rect(x - 20, y - 20, 70, 70)
+		c.fill()
+		c.restore()
 	}
 
-	def drawArrow (x: Int, y: Int, arrow: Image, orientation: Orientation)(implicit context: CanvasRenderingContext2D) = {
-		context.save()
-		context.beginPath()
-		context.translate(x + 15, y + 15);
+	def drawArrow (x: Int, y: Int, arrow: Image, orientation: Orientation)(implicit c: CanvasRenderingContext2D) = {
+		c.save()
+		c.beginPath()
+		c.translate(x + 15, y + 15);
 		val degrees = orientation match {
 			case North => 0
 			case East => 90
 			case South => 180
 			case West => 270
 		}
-		drawRotated(degrees, () => { context.drawImage(arrow, -30, -30, 60, 60) } )
-		context.restore()
+		drawRotated(degrees, () => { c.drawImage(arrow, -30, -30, 60, 60) } )
+		c.restore()
 	}
 
-	def drawRotated (degrees: Int, f: () => Unit)(implicit context: CanvasRenderingContext2D) = {
+	def drawRotated (degrees: Int, f: () => Unit)(implicit c: CanvasRenderingContext2D) = {
 		println(s"drawing at $degrees degrees")
-		context.rotate(degrees * Math.PI / 180)
+		c.rotate(degrees * Math.PI / 180)
 		f()
 	}
 }
